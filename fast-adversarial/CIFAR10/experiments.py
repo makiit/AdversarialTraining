60ea3a4c3632d2f029db9712ede33a945eaee6d8
makiit <mohammadali.khan13@gmail.com>
2022-02-06 12:04:10 -0800

    dSDS

--- fast-adversarial/CIFAR10/experiments.py
+++ fast-adversarial/CIFAR10/experiments.py
@@ -1,6 +1,6 @@
 import os
 out_dir = "train_pgd_"
-for i in range(5,10):
+for i in range(7,10):
     print("Running experiments for epsilon %d"%i)
     command = "python train_pgd.py --epsilon %d --out-dir %s"%(i,out_dir+str(i))
     os.system(command)
