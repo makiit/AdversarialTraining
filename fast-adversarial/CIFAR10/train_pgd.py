540d3d90492c9386a8ca47860dfee8945ea679d9
makiit <mohammadali.khan13@gmail.com>
2022-02-06 16:44:19 -0800

    dads

--- fast-adversarial/CIFAR10/train_pgd.py
+++ fast-adversarial/CIFAR10/train_pgd.py
@@ -40,6 +40,8 @@ def get_args():
         help='If loss_scale is "dynamic", adaptively adjust the loss scale over time')
     parser.add_argument('--master-weights', action='store_true',
         help='Maintain FP32 master weights to accompany any FP16 model weights, not applicable for O1 opt level')
+    parser.add_argument('--eval', default=False)
+    parser.add_argument('--checkpoint',default=None)
     return parser.parse_args()
 
 
@@ -68,70 +70,75 @@ def main():
     epsilon = (args.epsilon / 255.) / std
     alpha = (args.alpha / 255.) / std
 
-    model = PreActResNet18().cuda()
-    model.train()
+    if(args.eval==False):
+        model = PreActResNet18().cuda()
+        model.train()
 
-    opt = torch.optim.SGD(model.parameters(), lr=args.lr_max, momentum=args.momentum, weight_decay=args.weight_decay)
-    amp_args = dict(opt_level=args.opt_level, loss_scale=args.loss_scale, verbosity=False)
-    if args.opt_level == 'O2':
-        amp_args['master_weights'] = args.master_weights
-    model, opt = amp.initialize(model, opt, **amp_args)
-    criterion = nn.CrossEntropyLoss()
+        opt = torch.optim.SGD(model.parameters(), lr=args.lr_max, momentum=args.momentum, weight_decay=args.weight_decay)
+        amp_args = dict(opt_level=args.opt_level, loss_scale=args.loss_scale, verbosity=False)
+        if args.opt_level == 'O2':
+            amp_args['master_weights'] = args.master_weights
+        model, opt = amp.initialize(model, opt, **amp_args)
+        criterion = nn.CrossEntropyLoss()
 
-    lr_steps = args.epochs * len(train_loader)
-    if args.lr_schedule == 'cyclic':
-        scheduler = torch.optim.lr_scheduler.CyclicLR(opt, base_lr=args.lr_min, max_lr=args.lr_max,
-            step_size_up=lr_steps / 2, step_size_down=lr_steps / 2)
-    elif args.lr_schedule == 'multistep':
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(opt, milestones=[lr_steps / 2, lr_steps * 3 / 4], gamma=0.1)
+        lr_steps = args.epochs * len(train_loader)
+        if args.lr_schedule == 'cyclic':
+            scheduler = torch.optim.lr_scheduler.CyclicLR(opt, base_lr=args.lr_min, max_lr=args.lr_max,
+                step_size_up=lr_steps / 2, step_size_down=lr_steps / 2)
+        elif args.lr_schedule == 'multistep':
+            scheduler = torch.optim.lr_scheduler.MultiStepLR(opt, milestones=[lr_steps / 2, lr_steps * 3 / 4], gamma=0.1)
 
-    # Training
-    start_train_time = time.time()
-    logger.info('Epoch \t Seconds \t LR \t \t Train Loss \t Train Acc')
-    for epoch in range(args.epochs):
-        start_epoch_time = time.time()
-        train_loss = 0
-        train_acc = 0
-        train_n = 0
-        for i, (X, y) in enumerate(train_loader):
-            X, y = X.cuda(), y.cuda()
-            delta = torch.zeros_like(X).cuda()
-            if args.delta_init == 'random':
-                for i in range(len(epsilon)):
-                    delta[:, i, :, :].uniform_(-epsilon[i][0][0].item(), epsilon[i][0][0].item())
-                delta.data = clamp(delta, lower_limit - X, upper_limit - X)
-            delta.requires_grad = True
-            for _ in range(args.attack_iters):
+        # Training
+        start_train_time = time.time()
+        logger.info('Epoch \t Seconds \t LR \t \t Train Loss \t Train Acc')
+        for epoch in range(args.epochs):
+            start_epoch_time = time.time()
+            train_loss = 0
+            train_acc = 0
+            train_n = 0
+            for i, (X, y) in enumerate(train_loader):
+                X, y = X.cuda(), y.cuda()
+                delta = torch.zeros_like(X).cuda()
+                if args.delta_init == 'random':
+                    for i in range(len(epsilon)):
+                        delta[:, i, :, :].uniform_(-epsilon[i][0][0].item(), epsilon[i][0][0].item())
+                    delta.data = clamp(delta, lower_limit - X, upper_limit - X)
+                delta.requires_grad = True
+                for _ in range(args.attack_iters):
+                    output = model(X + delta)
+                    loss = criterion(output, y)
+                    with amp.scale_loss(loss, opt) as scaled_loss:
+                        scaled_loss.backward()
+                    grad = delta.grad.detach()
+                    delta.data = clamp(delta + alpha * torch.sign(grad), -epsilon, epsilon)
+                    delta.data = clamp(delta, lower_limit - X, upper_limit - X)
+                    delta.grad.zero_()
+                delta = delta.detach()
                 output = model(X + delta)
                 loss = criterion(output, y)
+                opt.zero_grad()
                 with amp.scale_loss(loss, opt) as scaled_loss:
                     scaled_loss.backward()
-                grad = delta.grad.detach()
-                delta.data = clamp(delta + alpha * torch.sign(grad), -epsilon, epsilon)
-                delta.data = clamp(delta, lower_limit - X, upper_limit - X)
-                delta.grad.zero_()
-            delta = delta.detach()
-            output = model(X + delta)
-            loss = criterion(output, y)
-            opt.zero_grad()
-            with amp.scale_loss(loss, opt) as scaled_loss:
-                scaled_loss.backward()
-            opt.step()
-            train_loss += loss.item() * y.size(0)
-            train_acc += (output.max(1)[1] == y).sum().item()
-            train_n += y.size(0)
-            scheduler.step()
-        epoch_time = time.time()
-        lr = scheduler.get_lr()[0]
-        logger.info('%d \t %.1f \t \t %.4f \t %.4f \t %.4f',
-            epoch, epoch_time - start_epoch_time, lr, train_loss/train_n, train_acc/train_n)
-    train_time = time.time()
-    torch.save(model.state_dict(), os.path.join(args.out_dir, 'model.pth'))
-    logger.info('Total train time: %.4f minutes', (train_time - start_train_time)/60)
+                opt.step()
+                train_loss += loss.item() * y.size(0)
+                train_acc += (output.max(1)[1] == y).sum().item()
+                train_n += y.size(0)
+                scheduler.step()
+            epoch_time = time.time()
+            lr = scheduler.get_lr()[0]
+            logger.info('%d \t %.1f \t \t %.4f \t %.4f \t %.4f',
+                epoch, epoch_time - start_epoch_time, lr, train_loss/train_n, train_acc/train_n)
+        train_time = time.time()
+        torch.save(model.state_dict(), os.path.join(args.out_dir, 'model.pth'))
+        logger.info('Total train time: %.4f minutes', (train_time - start_train_time)/60)
 
     # Evaluation
+
     model_test = PreActResNet18().cuda()
-    model_test.load_state_dict(model.state_dict())
+    if(args.eval==True):
+        model = torch.load(model_test,os.path.join(args.out_dir, 'model.pth'))
+    else:
+        model_test.load_state_dict(model.state_dict())
     model_test.float()
     model_test.eval()
 
