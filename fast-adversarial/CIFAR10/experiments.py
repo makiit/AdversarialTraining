562d50af561ee487d04dbd1e557ebaf767805115
makiit <mohammadali.khan13@gmail.com>
2022-02-08 03:57:43 -0800

    DSDD

--- fast-adversarial/CIFAR10/experiments.py
+++ fast-adversarial/CIFAR10/experiments.py
@@ -1,7 +1,7 @@
 import os
 import time
-c1 = "python train_pgd.py --alpha 2 --out-dir pgd8_alpha2 --epoch 20"
-c2 = "python train_pgd.py --alpha 4 --out-dir pgd8_alpha4 --epoch 20"
+c1 = "python train_pgd.py --alpha 2 --out-dir pgd8_alpha2"
+c2 = "python train_pgd.py --alpha 4 --out-dir pgd8_alpha4"
 c3 = "python train_pgd.py --restarts 2 --out-dir pgd8_restart2" 
 c4 = "python train_pgd.py --opt-level O0 --out-dir pgd8_fp32"
 c = [c1,c2,c3,c4]
