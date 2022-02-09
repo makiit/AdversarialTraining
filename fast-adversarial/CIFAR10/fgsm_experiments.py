f89d687724302ea6a91edb95f1adbad528779b62
makiit <mohammadali.khan13@gmail.com>
2022-02-09 00:32:44 -0800

    ALL

--- fast-adversarial/CIFAR10/fgsm_experiments.py
+++ fast-adversarial/CIFAR10/fgsm_experiments.py
@@ -1,12 +1,12 @@
 import os
 import time
-c1 = "python train_fgsm.p --delta-init zero --out-dir FGSM_DB/zero_init"
+c1 = "python train_fgsm.py --delta-init zero --out-dir FGSM_DB/zero_init"
 c2 = "python train_fgsm.py --early-stop  --delta-init zero --out-dir FGSM_DB/zero_init_early"
 c3 = "python train_fgsm.py --delta-init previous --out-dir FGSM_DB/prev_init"
 c4 = "python train_fgsm.py --delta-init random --alpha 8 --out-dir FGSM_DB/random_init_alpha8"
 c5 = "python train_fgsm.py --delta-init random  --out-dir FGSM_DB/random_init_alpha10"
 c6 = "python train_fgsm.py --delta-init random --alpha 16 --out-dir FGSM_DB/random_init_alpha16"
-c7 = "python train_fgsm.py --early-stop True --delta-init random --alpha 16 --out-dir FGSM_DB/random_init_alpha16_sarly"
+c7 = "python train_fgsm.py --early-stop --delta-init random --alpha 16 --out-dir FGSM_DB/random_init_alpha16_early"
 c = [c1,c2,c3,c4,c5,c6,c7]
 for x in c:
     print("Running ",x)
